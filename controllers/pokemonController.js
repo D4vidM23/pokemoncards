ca8568a421b3a918337ce8019d1bee42778a99f6
zxtt <zxtt@fresnou.org>
2022-12-08 10:59:01 -0800

    connectingloadingscreen

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,5 +1,19 @@
+
 module.exports.viewAll = function(req,res,next) {
-    const restaurant = {
-        id:1
-    }
+    const pokemons = {
+        id:1,
+        name: 'Unown',
+        numberhp: 40,
+        icon: '/images/phsychic_type-removebg-preview.png',
+        image: 'https://img.pokemondb.net/artwork/avif/unown.avif',
+        attack1: 'Tackle',
+        energycard: '/images/normal_type-removebg-preview.png',
+        damage: 20,
+        attack2: 'Hidden Power',
+        energycard2: ('/images/normal_type-removebg-preview.png','/images/phsychic_type-removebg-preview.png'),
+        damage2: 50,
+        energy4: '/images/phsychic_type-removebg-preview.png',
+        energy5: '/images/normal_type-removebg-preview.png',
+    };
+    res.render('index', {pokemons})
 }
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 const pokemonController = require('../controllers/pokemonController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', pokemonController.viewAll)
+
+
+// router.get('/', async function(req, res, next) {
+//   const {sequelize} = require("../models/index");
+//   const {QueryTypes} = require("sequelize");
+//
+//   let pokemoncards = await sequelize.query('select * from pokemontable', {type: QueryTypes.SELECT})
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
